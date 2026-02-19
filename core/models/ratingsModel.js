f411e7b7f4b2db50a722bfa3c0b39f8af7d292e7
aiwhoo <mattsap@aiwhoo.com8>
2026-02-19 12:12:11 -0500

    created the ratings model and validated that the stars needed be between 0 and 5 otherwise throw an exception.

---
+++ core/models/ratingsModel.js
@@ -0,0 +1,11 @@
+class RatingModel {
+    constructor(stars,  username,date, review="" ) {
+        this.stars = stars;
+        if(this.stars < 0 || this.stars > 5){
+            throw new Error("Stars must be between 0 and 5");
+        }
+        this.username = username;
+        this.date = date;
+        this.review = review;
+    }
+}
