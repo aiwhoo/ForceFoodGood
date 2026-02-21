8eeb5b2b023ef8130dbe6f6cc842c68dedcffe1d
idkanymore2009 <kadakiaaashi@gmail.com>
2026-02-21 14:17:23 -0500

    Implement CartItemModel for quantity validation
    added feature; create CartItemModel class to manage individual cart items and quantity validation in the cart.

--- core/models/cart model.js
+++ core/models/cart model.js
@@ -1,3 +1,16 @@
+class CartItemModel {
+    constructor(itemId, name, price, quantity = 1) {
+        if (quantity <= 0) {
+            throw new Error("Quantity must be greater than 0");
+        }
+
+        this.itemId = itemId;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
+
 class CartModel {
     constructor(restaurantId = null) {
         this.restaurantId = restaurantId;
