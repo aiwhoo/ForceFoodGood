97da87fcf0f271198a40d884cb4de4311ab274ee
Ian Porell <107158967+ianporell@users.noreply.github.com>
2026-02-23 15:53:48 -0500

    refactored  getCost, addMenuItem, removeMenuItem, and added documentation
    Changed getCost, addMenuItem, removeMenuItem such that cost is automatically recalculated after cart change, added inline comments

--- core/models/orderModel.js
+++ core/models/orderModel.js
@@ -1,5 +1,10 @@
 class OrderModel {
-
+    /*
+    username: string
+    address: string
+    restaurant: RestaurantModel
+    id: string
+    */
     constructor(username = "NO USERNAME",
                 address = "NO ADDRESS",
                 restaurant = "NO RESTAURANT",
@@ -13,45 +18,52 @@ class OrderModel {
         this.id = id
     }
 
-
+    // calculates the cost of the order after adding item to cart
     updateCost() {
         for (let i = 0; i < this.itemsOrdered.length; i++) {
             this.totalCost += this.itemsOrdered[i].price;
         }
     }
 
-
+    // returns number of items in cart
     getTotalItems() {
         return this.totalItems = this.itemsOrdered.length;
     }
 
+    // returns username of customer
     getUserName() {
         return this.username;
     }
 
+    // returns delivery address
     getUserAddress() {
         return this.address;
     }
 
+     // returns restaurant being ordered from
     getRestaurant() {
         return this.restaurant;
     }
 
+    // returns unique string ID of the order
     getId() {
         return this.id;
     }
 
+    // returns cost of the order
     getCost() {
-        this.updateCost();
         return this.totalCost;
     }
 
+    // adds an item to the cart
     addMenuItem(aMenuItem) {
         this.itemsOrdered.push(aMenuItem);
+        this.updateCost();
     }
-
+    // removes an item from the cart
     removeMenuItem(aMenuItem) {
         this.itemsOrdered.splice(this.itemsOrdered.indexOf(aMenuItem), 1);
+        this.updateCost();
     }
 
 }
