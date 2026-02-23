4b6f6ce67d064d88fcc3784f83b5164fce56b942
CSProject <adityarustagi200@gmail.com>
2026-02-23 14:58:03 -0500

    Add updateRating method to RestaurantModel

--- core/models/restaurantModel.js
+++ core/models/restaurantModel.js
@@ -3,31 +3,52 @@ class RestaurantModel {
         this.name = name;
         this.address = address;
         this.reviews = [];
+        this.rating = 0; // new property to store average rating
     }
+
     getName() {
         return this.name;
     }
+
     getAddress() {
         return this.address;
     }
+
     setAddress(address) {
         this.address = address;
     }
+
     setName(name) {
         this.name = name;
     }
-    calculateAverageReviewRating(){
+
+    calculateAverageReviewRating() {
         let sumOfReviews = 0;
-        for(let i = 0; i < this.reviews.length; i++){
-            sumOfReviews += this.reviews[i].stars
+
+        for (let i = 0; i < this.reviews.length; i++) {
+            sumOfReviews += this.reviews[i].stars;
         }
-        if(this.reviews.length == 0){
+
+        if (this.reviews.length == 0) {
             return "No Reviews Found";
         }
+
         return sumOfReviews / this.reviews.length;
     }
-    addReview(review){
+
+    addReview(review) {
         this.reviews.push(review);
     }
 
+    updateRating() {
+        const average = this.calculateAverageReviewRating();
+
+        if (average === "No Reviews Found") {
+            this.rating = 0;
+        } else {
+            this.rating = average;
+        }
+
+        return this.rating;
+    }
 }
