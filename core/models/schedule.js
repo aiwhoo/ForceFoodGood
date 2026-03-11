1fc50a70310e6e684e9d41bb265f20816e0e2532
KaiFu7k <kaifu7k@gmail.com>
2026-03-10 23:33:07 -0400

    schedule javascript added (parent class), and 3 (deliveryslot, driver, and restaurant) subclass in.

---
+++ core/models/schedule.js
@@ -0,0 +1,46 @@
+export class Schedule {
+    constructor(id) {
+        this.id = id;
+    }
+    isAvailable(param) {
+        console.log("Checking for the general availability...");
+        return false;
+    }
+}
+
+//first subclass with the restaurant
+export class RestaurantSchedule extends Schedule {
+    constructor(id, openTime, closeTime) {
+        super(id);
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+    }
+    isAvailable(time) {
+        return time >= this.openTime && time <= this.closeTime;
+    }
+}
+
+// second subclass with the driver
+export class DriverSchedule extends Schedule {
+    constructor(id, shiftStart, shiftEnd) {
+        super(id);
+        this.shiftStart = shiftStart;
+        this.shiftEnd = shiftEnd;
+        this.onBreak = false;
+    }
+    isAvailable(time) {
+        if (this.onBreak) return false;
+        return time >= this.shiftStart && time <= this.shiftEnd;
+    }
+}
+
+// third subclass with the deliveryslot
+export class DeliverySlot extends Schedule {
+    constructor(id) {
+        super(id);
+        this.isBooked = false;
+    }
+    isAvailable() {
+        return !this.isBooked;
+    }
+}
